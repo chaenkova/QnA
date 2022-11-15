92de5b0f83722c9dab62004d3be1196a238e6c48
chaenkova <dash.kustowa2010@yandex.ru>
2022-11-16 01:13:50 +0200

    turbo

--- app/javascript/answers.js
+++ app/javascript/answers.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+$(document).on('turbo:load', function(){
     $('.answers').on('click', '.edit-answer-link', function(e) {
         e.preventDefault();
         $(this).hide();
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+import "./answers"
+$(document).on('turbo:load', function() {
+    console.log('turbo loaded!');
+});
