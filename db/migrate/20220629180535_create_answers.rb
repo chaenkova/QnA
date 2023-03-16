0576978973f17c8c5db526c4e2100c9477fe0f49
chaenkova <dash.kustowa2010@yandex.ru>
2023-03-16 08:30:48 +0300

    small fix

--- db/migrate/20220629180535_create_answers.rb
+++ db/migrate/20220629180535_create_answers.rb
@@ -1,8 +1,8 @@
 class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
     create_table :answers do |t|
-      t.text :body
-      t.references :question, foreign_key: true, null: false
+      t.text :body, null: false
+      t.belongs_to :question, foreign_key: true, null: false
 
       t.timestamps
     end
