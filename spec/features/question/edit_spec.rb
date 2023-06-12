9cd97a349b7b4c3f39839d359c747874aff876a7
chaenkova <dash.kustowa2010@yandex.ru>
2023-06-12 15:48:46 +0300

    edit question+fix tests

--- spec/features/question/edit_spec.rb
+++ spec/features/question/edit_spec.rb
@@ -5,42 +5,48 @@ feature 'User can edit question', %q{
   As an authenticated user
   I'd like to be able to edit the question
 }do
-  given(:user) {create(:user)}
-  given(:question) {create(:question)}
+  given!(:user) {create(:user)}
+  given!(:question) {create(:question,user: user)}
+  given(:user2) {create(:user)}
   describe 'Authenticated user' do
     background do
       sign_in(user)
 
-      visit questions_path(question)
+      visit question_path(question)
     end
 
-    scenario 'edit own question' do
+    scenario 'edit own question', js: true do
+
+      within '.question' do
       click_on 'Edit'
       fill_in 'Title', with: 'Test question'
       fill_in 'Body', with: 'Test text'
-      click_on 'Ask'
-
-      expect(page).to have_content 'Your question successfully created'
+      click_on 'Save'
+      end
       expect(page).to have_content 'Test question'
       expect(page).to have_content 'Test text'
     end
 
 
-    scenario 'edit own a question with errors' do
+    scenario 'edit own a question with errors', js: true do
 
-      click_on 'Ask'
+      within '.question' do
+        click_on 'Edit'
+        fill_in 'Title', with: ''
+
+        click_on 'Save'
+
+      end
       expect(page).to have_content "Title can't be blank"
     end
-
-    scenario "tries to edit other user's question", js: true do
-      sign_in user2
-      visit question_path(question)
-
-      expect(page).to_not have_link 'Edit'
-    end
   end
-  scenario 'Unauthenticated user tries edit a question' do
+  scenario "tries to edit other user's question", js: true do
+    sign_in user2
+    visit question_path(question)
 
+    expect(page).to_not have_link 'Edit'
+  end
+  scenario 'Unauthenticated user tries edit a question', js: true do
     visit questions_path
     click_on 'Ask question'
 
