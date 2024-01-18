72b90e42be9a833d821a02f1baf43dfb1a09affd
chaenkova <dash.kustowa2010@yandex.ru>
2024-01-18 21:25:31 +0300

    fix comments

--- app/assets/javascripts/channels/comments_channel.js
+++ app/assets/javascripts/channels/comments_channel.js
@@ -2,6 +2,8 @@ $(document).on('turbolinks:load', function(){
     App.cable.subscriptions.create({ channel: 'CommentsChannel', question_id: gon.question_id }, {
         connected() {
             console.log('Client connected to comments channel')
+            console.log(gon.question_id)
+            this.perform('follow');
         },
 
         disconnected() {
@@ -11,16 +13,20 @@ $(document).on('turbolinks:load', function(){
         received(content) {
             console.log('received')
             console.log(content)
-            if (gon.user_id !== content.author.id) {
-                let commentClass = content.type + '-id-' + content.type_id + '-comments'
+            if (gon.user_id !== content.comment.user_id) {
+                let commentClass = content.comment.commentable_type + '-id-' + content.comment.commentable_id + '-comments'
                 console.log(commentClass)
                 $('.' + commentClass).append(JST["templates/comments"]({
                     comment: content.comment,
-                    author: content.author
+                    author: content.user
                 }));
             } else {
                 console.log('Comment already on page')
             }
+        },
+
+        follow: function() {
+            return this.perform('follow');
         }
     })
 })
